5389b5c9170940806bdc0df677b8d8d7d11aa6fc
hamed.nabatpour <hamed.np.fhy91@gmail.com>
2023-06-23 01:52:11 +0330

    add text setter to Message class

--- src/main/java/com/example/model/entity/Message.java
+++ src/main/java/com/example/model/entity/Message.java
@@ -11,6 +11,10 @@ public class Message {
 
     private String text;
 
+    public void setText(String text) {
+        this.text = text;
+    }
+
     public String getText() {
         return text;
     }
